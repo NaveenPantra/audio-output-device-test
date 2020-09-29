57ba90d08f13c0696d37846dc66109c64e4fb0b7
naveen-plivo <68113002+naveen-plivo@users.noreply.github.com>
2020-09-29 18:02:24 +0530

    Audio Output device

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import audioFile from "./audio.mp3";
 
 function App() {
+  const [devices, setDevices] = useState([]);
+  const audio = useRef();
+
+  useEffect(() => {
+    getMediaDevices();
+  }, []);
+
+  useEffect(() => {
+    console.log(devices);
+  }, [devices]);
+
+  function getMediaDevices() {
+    try {
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then(() => {
+          navigator.mediaDevices
+            .enumerateDevices()
+            .then(setDevices)
+            .catch((err) => {
+              console.error(err);
+            });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } catch (_) {}
+  }
+
+  function handleOnChnage(event) {
+    audio.current.setSinkId(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <audio src={audioFile} controls ref={audio} loop />
+      <select onChange={handleOnChnage}>
+        {devices
+          .filter((device) => device.kind === "audiooutput")
+          .map((device) => (
+            <option key={device.deviceId} value={device.deviceId}>
+              {device.label}
+            </option>
+          ))}
+      </select>
     </div>
   );
 }
